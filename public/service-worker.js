37988008bdc8a57b8c7465af1651880a69f0c6b4
mtbishop <bishopmatthew76@gmail.com>
2021-04-30 22:35:35 -0500

    added returning cache to api route

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,12 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png',
   '/index.js',
 ];
+
+const CACHE = 'cache-v1';
+const DATA_CACHE = 'data-cache-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(DATA_CACHE).then((value) => value.add('/api/transaction'))
+  );
+});
