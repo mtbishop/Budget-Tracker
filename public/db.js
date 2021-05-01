d830159905d366098435af9b9dc2407f26df708d
mtbishop <bishopmatthew76@gmail.com>
2021-04-30 21:33:30 -0500

    creating db functions

--- public/db.js
+++ public/db.js
@@ -1,11 +1,50 @@
 let db;
 
-const request = indexedDB.open("budget_db", 1);
+const request = indexedDB.open('budget_db', 1);
 
+// Will trigger if db doesn't exist
 request.onupgradeneeded = function (e) {
-    db = e.target.result;
-    const { version1 } = e;
-    const version2 = e.version2 || db.version;
+  db = e.target.result;
 
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('MoneyStore', { autoIncrement: true });
+  }
+};
 
+//Error request
+request.onerror = function (e) {
+  console.log('Error loading database.');
+};
+
+// On success
+request.onsuccess = function () {
+  db = target.result;
+  if (navigator.onLine) {
+    checkDB();
+  }
+};
+// function
+function checkDB() {
+  const store = transaction.objectStore('pending');
+  const retrieved = store.getAll();
+  const transaction = db.transaction(['pending...'], 'readwrite');
+  // onsuccess it will take you to the url on 34 and display the results of the database
+  retrieved.onsuccess = function () {
+    if (retrieved.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(retrieved.result),
+        headers: {
+          Accept: "application/json, text/plain, '/'",
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then(() => {
+          store.clear();
+        });
+    }
+  };
 }
