a86530c6e68c02b6e9e54e23eafd8e98f4268c87
mtbishop <bishopmatthew76@gmail.com>
2021-05-02 18:28:45 -0500

    added only if cache if condition

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,23 @@
 const CACHE = 'cache-v1';
 const DATA_CACHE = 'data-cache-v1';
 
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('static').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'styles.css',
+        'index.js',
+        'icons/icon-192x192.png',
+        'icons/icon-512x512.png',
+        'manifest.webmanifest',
+      ]);
+    })
+  );
 
-  self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open('static').then((cache) => {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'styles.css',
-          'index.js',
-          'icons/icon-192x192.png',
-          'icons/icon-512x512.png',
-          'manifest.webmanifest',
-        ]);
-      })
-    );
-
-    self.skipWaiting();
-  });
-  
-  
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(
@@ -61,6 +58,12 @@ self.addEventListener('fetch', (e) => {
   }
   e.respondWith(
     caches.open(CACHE).then((c) => {
+      if (
+        e.request.cache === 'only-if-cached' &&
+        e.request.mode !== 'same-origin'
+      ) {
+        return;
+      }
       return c.match(e.request).then((response) => {
         return response || fetch(e.request);
       });
